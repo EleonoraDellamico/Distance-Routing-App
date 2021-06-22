a97faaa1db98e371d953c6f681eb5956b0aae188
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2021-06-22 16:28:20 +0200

    added map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useEffect, useState, useRef } from 'react';
 import './App.css';
+import * as tt from '@tomtom-international/web-sdk-maps';
 
-function App() {
+const App = () => {
+	const mapElement = useRef();
+	const [ map, setMap ] = useState({});
+	useEffect(() => {
+		let map = tt.map({
+			key: process.env.REACT_APP_KEY_TOM_TOM,
+			container: mapElement.current
+		});
+		setMap(map);
+	}, []);
 	return (
 		<div className="App">
-			<h1>Eli</h1>
+			<div ref={mapElement} />
 		</div>
 	);
-}
+};
 
 export default App;
