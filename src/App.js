91a0aa4794b9dd6b04c046ca035fe56ef8ca636a
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2021-06-23 10:56:32 +0200

    set map to Madrid

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ import * as tt from '@tomtom-international/web-sdk-maps';
 const App = () => {
 	const mapElement = useRef();
 	const [ map, setMap ] = useState({});
+	const [ longitude, setLongitude ] = useState(-3.6844995);
+	const [ latitude, setLatitude ] = useState(40.4152606);
+
 	useEffect(() => {
 		let map = tt.map({
 			key: process.env.REACT_APP_KEY_TOM_TOM,
-			container: mapElement.current
+			container: mapElement.current,
+			center: [ longitude, latitude ],
+			zoom: 14
 		});
 		setMap(map);
 	}, []);
 	return (
 		<div className="App">
-			<div ref={mapElement} />
+			<div ref={mapElement} className="map" />
 		</div>
 	);
 };
