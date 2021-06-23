5f44ba75432d572098fc41be4b2fa32bd1144d01
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2021-06-23 11:27:53 +0200

    added a marker

--- src/App.js
+++ src/App.js
@@ -8,18 +8,53 @@ const App = () => {
 	const [ longitude, setLongitude ] = useState(-3.6844995);
 	const [ latitude, setLatitude ] = useState(40.4152606);
 
-	useEffect(() => {
-		let map = tt.map({
-			key: process.env.REACT_APP_KEY_TOM_TOM,
-			container: mapElement.current,
-			center: [ longitude, latitude ],
-			zoom: 14
-		});
-		setMap(map);
-	}, []);
+	useEffect(
+		() => {
+			let map = tt.map({
+				key: process.env.REACT_APP_KEY_TOM_TOM,
+				container: mapElement.current,
+				stylesVisibility: {
+					trafficIncidents: true,
+					trafficFlow: true
+				},
+				center: [ longitude, latitude ],
+				zoom: 14
+			});
+			setMap(map);
+			const addMarker = () => {
+				const marker = new tt.marker({
+					draggable: true,
+					element: element
+				});
+			};
+			return () => map.remove();
+		},
+		[ longitude, latitude ]
+	);
 	return (
 		<div className="App">
 			<div ref={mapElement} className="map" />
+			<div className="search-bar">
+				<h1>Where to?</h1>
+				<input
+					type="text"
+					id="longitude"
+					className="longitude"
+					placeholder="Put in longitude"
+					onChange={(e) => {
+						setLongitude(e.target.value);
+					}}
+				/>
+				<input
+					type="text"
+					id="latitude"
+					className="latitude"
+					placeholder="Put in latitude"
+					onChange={(e) => {
+						setLatitude(e.target.value);
+					}}
+				/>
+			</div>
 		</div>
 	);
 };
